4468a951c0a836d95996cf0b773aab2e9a491c41
Alex34972 <alex_hifi@ukr.net>
2021-07-25 19:51:44 +0300

    Update Phonebook.js

--- src/components/Phonebook.js
+++ src/components/Phonebook.js
@@ -1,17 +1,20 @@
-import React from "react";
-import shortid from "shortid";
+import React from 'react';
+import shortid from 'shortid';
 class Phonebook extends React.Component {
   state = {
     contacts: [],
-    name: "",
-    number: "",
+    name: '',
+    number: '',
   };
 
   nameInputId = shortid.generate();
   numberInputId = shortid.generate();
-  handleChange = (event) => {
+  handleChange = event => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
+  };
+  reset = () => {
+    this.setState({ name: '', number: '' });
   };
   handleAppend = () => {
     const { name, number } = this.state;
@@ -20,47 +23,56 @@ class Phonebook extends React.Component {
       name: name,
       number: number,
     };
-    this.setState((prevSate) => ({
+    console.log(contact, name.length, number.length);
+    this.setState(prevSate => ({
       contacts: [contact, ...prevSate.contacts],
     }));
+    this.reset();
   };
   render() {
     const { name, number, contacts } = this.state;
     return (
       <>
         <h2>Phonebook</h2>
-        <p>Name</p>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-          required
-          value={name}
-          id={this.nameInputId}
-          onChange={this.handleChange}
-        />
-        <input
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-          required
-          value={number}
-          id={this.numberInputId}
-          onChange={this.handleChange}
-        />
+        <form>
+          <p>Name</p>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+            value={name}
+            id={this.nameInputId}
+            onChange={this.handleChange}
+          />
+          <p>Number</p>
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+            value={number}
+            id={this.numberInputId}
+            onChange={this.handleChange}
+          />
 
-        <button type="button" onClick={this.handleAppend}>
-          Add contact
-        </button>
+          <button type="button" onClick={this.handleAppend}>
+            Add contact
+          </button>
+        </form>
         <h2>Contacts</h2>
         <ul>
-          {contacts.map(({ id, name, number }) => (
-            <li key={id}>
-              {name}:{number}
-            </li>
-          ))}
+          {contacts.map(
+            ({ id, name, number }) =>
+              name.length > 0 &&
+              number.length > 0 && (
+                <li key={id}>
+                  {name}:{number}{' '}
+                </li>
+              ),
+          )}
         </ul>
       </>
     );
